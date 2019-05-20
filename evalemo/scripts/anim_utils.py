d8cb55860840bd1dde3e335ede9e22a793dcff05
mina <mina.marmpena@gmail.com>
2019-05-20 19:47:33 +0200

    change for vae

---
+++ evalemo/scripts/anim_utils.py
@@ -0,0 +1,49 @@
+import qi
+import sys, os
+import time
+import pandas as pd
+from anim_settings import *
+
+
+def naoqi_connect(strIP='10.0.206.62', nPort=9559):
+
+    naoqi_session = qi.Session()
+    try:
+        naoqi_session.connect("tcp://" + strIP + ":" + str(nPort))
+        return naoqi_session
+    except RuntimeError:
+        print("Can't connect to Naoqi")
+        sys.exit(1)
+
+
+def go_2_init(posture_ses):
+    posture_ses.goToPosture("StandInit", 0.15)
+
+
+def load_modules(session):
+    motion_ses = session.service("ALMotion")
+    aplayer_ses = session.service("ALAnimationPlayer")
+    posture_ses = session.service("ALRobotPosture")
+    leds_ses = session.service("ALLeds")
+    alife_ses = session.service("ALAutonomousLife")
+
+    return motion_ses, aplayer_ses, posture_ses, leds_ses, alife_ses
+
+
+def init_rest(leds_ses, alife_ses, posture_ses):
+    leds_ses.reset("FaceLeds")
+    go_2_init(posture_ses)
+    alife_ses.setAutonomousAbilityEnabled("BasicAwareness", False)
+    alife_ses.setAutonomousAbilityEnabled("ListeningMovement", False)
+    alife_ses.setAutonomousAbilityEnabled("SpeakingMovement", False)
+    alife_ses.setAutonomousAbilityEnabled("BackgroundMovement", False)
+    autoState = alife_ses.getState()
+    if autoState != "disabled":  # disable
+        alife_ses.setState("disabled")
+        posture_ses.goToPosture("StandInit", 0.6)
+
+
+def divide_chunks(l, n):
+    # looping till length l
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
---
+++ evalemo/scripts/play_df_cond_gaussian_sampling.py
@@ -0,0 +1,53 @@
+from anim_utils import *
+from animDict import gen_anim_list
+
+"""
+Load a df of postures and display on the simulator without interpolation.
+For conditioned animations. Both grid and gaussian sampling. VA or V_only
+
+"""
+
+
+def main(motion_ses, leds_ses, idAnim):
+
+    nameAnim = gen_anim_list[idAnim]
+    script_dir = os.path.dirname(__file__)
+    rel_path = 'naoqi_data'
+    anim_dir = os.path.join(script_dir, rel_path, nameAnim)
+
+    start = time.time()
+    df = pd.read_csv(anim_dir, index_col=0, skipinitialspace=True)
+
+    df = df.astype('float')
+    if 'valence' in df.columns:
+        valence = df['valence'].mean()
+        print("Valence: " + str(valence))
+    print("Anim: " + nameAnim)
+
+    # TODO: uncomment after you drop everything excepts joints and leds
+    # Add StandInit frame at the end of the animation
+    # last_idx = len(df)
+    # df.loc[last_idx, joints_names] = standInit
+    # df.loc[last_idx, leds_keys] = 1
+
+    df['KneePitch'] = -1.49414
+    motion_ses.setStiffnesses("WholeBody", 1)
+
+    # Play animation
+    for i in range(df.shape[0]):
+
+        angle_list = df.loc[i, joints_names].tolist()
+        leds_list = df.loc[i, leds_keys].tolist()
+        rgb_chunks = [leds_list[x * 3:(x + 1) * 3] for x in range((len(leds_list) + 3 - 1) // 3)]
+
+        # 16 LEDs, with a 3 RGBs
+        for l in range(16):
+            leds_ses.fadeRGB(leds_short[l], rgb_chunks[l][0], rgb_chunks[l][1], rgb_chunks[l][2], 0., _async=True)
+
+        # Motion
+        motion_ses.setAngles(joints_names, angle_list, 0.05)
+
+    print('Duration: ' + str(time.time() - start))
+
+    return nameAnim
+
