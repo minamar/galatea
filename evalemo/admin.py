ba600c70001cdefc42e5c70db84cf0de90ff3696
mina <mina.marmpena@gmail.com>
2019-05-24 17:30:40 +0200

    register survey

--- evalemo/admin.py
+++ evalemo/admin.py
@@ -4,7 +4,8 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
-from models import Judge, Demographic
+from models import Judge, Demographic, Survey
 
 admin.site.register(Judge)
 admin.site.register(Demographic)
+admin.site.register(Survey)
